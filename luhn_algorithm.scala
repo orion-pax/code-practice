1dc10e9737782368d2af3f3ca997ba69376bf583
tafadzwac <orion-pax@users.noreply.github.com>
2022-04-26 15:27:01 +0200

    Create luhn_algorithm.scala
    adding implementatin of Luhn algorithm in Scala

---
+++ luhn_algorithm.scala
@@ -0,0 +1,23 @@
+object MyClass {
+
+    def main(args: Array[String]) {
+        val checksum = luhnAlgorithm("79927398713")
+    }
+    
+    def luhnAlgorithm(idNumber: String): Boolean = {
+        var sum = 0
+        var isSecond = false
+        for (i <- 0 to idNumber.length - 1) {
+          var digit = idNumber.charAt(i).getNumericValue
+          if (isSecond) { 
+            digit = digit * 2
+            if (digit > 9) {
+              digit = digit - 9 
+            }
+          }
+          sum += digit
+          isSecond = !isSecond
+        }
+        sum % 10 == 0
+  }    
+}
